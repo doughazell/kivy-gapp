91c9c89cc81a587cc4acb46f3be3f23da806b01b
Doug Hazell <doughazell@gmail.com>
2018-05-22 21:11:56 +0100

    Update for displaying col order as specified

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.properties import ListProperty
 from kivy.uix.label import Label
 from kivy.uix.carousel import Carousel
+from kivy.uix.textinput import TextInput
 
 # 8/9/17 DH: Changing button text and sleeping prior to reverting it
 import time
@@ -35,17 +38,33 @@ def populateCarousel(self):
 
         self.sheet = client.open("Addresses").worksheet("Personal")
 
+        # 8/3/18 DH: Adding in display details stored within the sheet
+        order = self.sheet.cell(1,8)
+        print('Col order: ' + order.value)
+        self.txt1.text = order.value
+
         list_of_dicts = self.sheet.get_all_records()
         record_num = len(list_of_dicts)
         self.lbl1.text = str(record_num) + ' records'
 
+        # === Rows ===
         for idx in range(0,record_num):
             lbl = Label()
             values = self.sheet.row_values(idx+2)
             if values:
-                for i in range(5):
-                    value = values.pop(2)
-                    lbl.text += value + '\n'
+                # ||| Cols |||
+                # 22/3/18 DH: Selected cols added as specified in TextInput 'txt1'
+                colsIndexed = dict(zip(range(1,8), values))
+
+                cols = self.txt1.text.split(",")
+                for col in cols:
+                    lbl.text += colsIndexed.get(int(col)) + '\n'
+
+                #for i in range(5):
+                #    value = values.pop(2)
+                #    lbl.text += value + '\n'
+
+                # Add record to carousel
                 #print "Adding: " + lbl.text
                 self.add_widget(lbl)
 
@@ -56,74 +75,30 @@ def populateCarousel(self):
         self.lbl1.text='Error with Google Sheets!'
 
 
-#class RootWidget(BoxLayout):
-#class RootWidget(GridLayout):
 class RootWidget(Carousel):
 
     def __init__(self, **kwargs):
 
         super(RootWidget, self).__init__(**kwargs)
 
-        # ---------------------- NUI Functionality -----------------------
-        btn1 = Button(text='btn 1')
-        # 17/6/17 DH: https://kivy.org/docs/api-kivy.uix.behaviors.button.html
-        #   so 'on_press' rather than 'on_pressed' as alluded to in https://kivy.org/docs/guide/events.html
-        btn1.bind(on_press=self.btn1_pressed)
-        btn1.bind(on_release=self.btn1_released)
-        self.btn1=btn1
-
-        cb = CustomBtn()
-        # 19/8/17 DH: Testing override of 'on_pressed'
-        #cb.bind(mypressed=self.btn_pressed)
-        cb.bind(pressed=self.cstbtn_pressed)
-
-        btn2 = Button(text='btn 2')
-        btn2.bind(on_press=self.btn2_pressed)
-        btn2.bind(on_release=self.btn2_released)
-
-        # ---------------------- NUI Layout ----------------------
-        layout1 = BoxLayout(spacing=10, size_hint_y=None, height=50)
-        lbl1 = Label(text='Yea baby...!', color=red)
-        self.lbl1 = lbl1
-        layout1.add_widget(lbl1)
-
-        layout2 = BoxLayout(spacing=10)
-        layout2.add_widget(btn1)
-        layout2.add_widget(cb)
-        layout2.add_widget(btn2)
-
-        # 24/2/18 DH: GridLayout
-        grid = GridLayout(rows=2)
-        grid.add_widget(layout1)
-        grid.add_widget(layout2)
+        # 14/4/18 DH: Shift to kv file
+        #btn1 = ObjectProperty(None)
+        # 21/5/18 DH: next step to shift...
+        txt1 = ObjectProperty(None)
+        lbl1 = ObjectProperty(None)
 
         # 24/2/18 DH: Carousel
         self.direction='right'
 
-        # 24/2/18 DH: Initially add the start screen then add the table rows later
-        self.add_widget(grid)
-
         populateCarousel(self)
+
+    def on_enter(instance, txtinput):
+        print('Order: ' + txtinput.text)
 
     def cstbtn_pressed(self, instance, pos):
         #print ('pos: printed from root widget: {pos}'.format(pos=pos))
         self.btn1.text='pos: {pos}'.format(pos=pos)
 
-    def btn1_pressed(self, instance):
-        #print ('yay...:)')
-
-        instance.text="Hey totally...3"
-        #time.sleep(1)
-        #print ('yay2...:)')
-
-    def btn1_released(self, instance):
-        instance.text="btn 1"
-
-    '''
-    def btn2_pressed(self, instance):
-        instance.text="Google Sheets coming...:)"
-
-    '''
     def btn2_pressed(self, instance):
         try:
             list_of_dicts = self.sheet.get_all_records()
@@ -178,11 +153,11 @@ class CustomBtn(Widget):
     def on_pressed(self, instance, pos):
         print ('pressed at {pos}'.format(pos=pos))
 
-class TestApp(App):
+class SheetsApp(App):
 
     def build(self):
         return RootWidget()
 
 
 if __name__ == '__main__':
-    TestApp().run()
+    SheetsApp().run()
